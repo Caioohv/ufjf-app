8c5a5dadbc96468960f8d3f48aab8b55ca343834
Kaymartins <kayanmartins1@gmail.com>
2023-07-12 06:36:28 -0300

    faz pagina estatica de informações da ufjf

--- app/src/main/java/com/example/guiacalouros/InfoUfjf.java
+++ app/src/main/java/com/example/guiacalouros/InfoUfjf.java
@@ -2,26 +2,22 @@ package com.example.guiacalouros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import android.content.Intent;
-import android.icu.text.IDNA;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
+public class InfoUfjf extends AppCompatActivity {
 
 
-public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_info_ufjf);
 
     }
 
-    public void redirectUfjfPage(View view){
-        this.startActivity(new Intent(this,InfoUfjf.class));
+    public void returnHome(View view){
+        this.startActivity(new Intent(this, MainActivity.class));
     }
-
-
 }

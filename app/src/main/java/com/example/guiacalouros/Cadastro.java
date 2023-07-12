883e049a4908bd4c3d7710aa481ce58a773180b9
João Victor Senra <joaovictor.senra@estudante.ufjf.br>
2023-07-12 00:05:22 -0300

    Add classes GuiaUser e UserDAO

--- app/src/main/java/com/example/guiacalouros/Cadastro.java
+++ app/src/main/java/com/example/guiacalouros/Cadastro.java
@@ -1,8 +1,8 @@
 package com.example.guiacalouros;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
 
 public class Cadastro extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/guiacalouros/GuiaUser.java
@@ -0,0 +1,41 @@
+package com.example.guiacalouros;
+
+public class GuiaUser {
+    private String name;
+    private String Cpf;
+    private String cpf;
+    private String email;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return Cpf;
+    }
+
+    public void setCpf(String cpf) {
+        Cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
